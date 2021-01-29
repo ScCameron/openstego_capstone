33433f5ac9a3f08ec6195fefeda91e6cc3e1b596
Scott Cameron <camerons46@mymacewan.ca>
2021-01-28 17:59:55 -0700

    Revert "Revert "remove old files""
    This reverts commit 0d4ecc0cf7688b24376f71a28a4086f8256c8c27.

---
+++ parser/tests/if.go
@@ -0,0 +1,13 @@
+package main;
+
+func main() {
+	if i == 7 + 3 {
+		if i == 7 + 3 {
+			if i == 7 + 3 {
+				if i == 7 + 3 {
+					i = 0;
+				};
+			};
+		};
+	};
+};
---
+++ parser/tests/printread.go
@@ -0,0 +1,12 @@
+package main;
+
+
+func main() {
+	var i int;
+	i = 0;
+	println(i);
+	println(i, i, i);
+	readln(&i);
+	readln(&i, &j, &k);
+};
+
